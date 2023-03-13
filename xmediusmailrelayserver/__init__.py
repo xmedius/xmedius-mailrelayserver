d64fc4786d0f9832be5b6b81cd26ea1c704c92c8
Laurent Sabourin <lsabourin@opentext.com>
2023-03-13 12:11:33 -0400

    XMXC-22315 - xmedius-mailrelayserver fails to start with Python 3.10
    Enable WindowsSelectorEventLoopPolicy to allow running the event
    loop on the main thread.

--- xmediusmailrelayserver/__init__.py
+++ xmediusmailrelayserver/__init__.py
@@ -1,2 +1,6 @@
+import sys
+import asyncio
 from xmediusmailrelayserver import *
 
+if sys.platform == "win32" and sys.version_info >= (3, 8, 0):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
